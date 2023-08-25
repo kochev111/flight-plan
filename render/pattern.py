375b554096bd34ecaea2e806c15bcee4caf53f11
Victor Kochemirovskiy <victor.kochemirovskiy@gmail.com>
2023-08-25 20:31:26 +0100

    add disclaimer

--- render/pattern.py
+++ render/pattern.py
@@ -121,7 +121,7 @@ class Pattern:
         return PLA
 
     def make_text(self):
-        text = "DISCLAIMER: FLIGHT PLAN IS FOR ILLUSTRATION PURPOSES ONLY. NO LIABILITIES ASSUMED, EXPRESS OR IMPLIED."
+        text = "DISCLAIMER: FLIGHT PLAN IS FOR ILLUSTRATION PURPOSES ONLY. NO LIABILITIES ASSUMED.\n"
         text += "Winds:\n"
         alts = self.altitudes_ft
         winds = self.forecast.get_forecast(self.altitudes_m)
@@ -147,7 +147,7 @@ class Pattern:
 
         text += "\n"
         text += "Generated " + dt.datetime.now().strftime("%d %B %Y %H:%M:%S") + "\n"
-        text += "Based on open-meteo weather forecast 30min ahead - always check actual weather\n"
+        text += "Based on open-meteo weather forecast 30min ahead\n"
 
         return text
 
